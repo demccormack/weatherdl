2c55703d53d9d15c0fde659bfe0c1f530fc99aa2
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2022-11-12 16:20:30 +1300

    Autoformat with autopep8 and isort

--- src/weatherdl.py
+++ src/weatherdl.py
@@ -1,11 +1,13 @@
 import json
-import requests
 from datetime import datetime, timedelta
-from os import getcwd, mkdir, path
-from magic import from_buffer
-from pytz import timezone
 from glob import glob
+from os import getcwd, mkdir, path
+
+import requests
+from magic import from_buffer
 from pptx import Presentation
+from pytz import timezone
+
 
 class downloader(object):
 
@@ -15,7 +17,8 @@ class downloader(object):
         config_file.close()
         self.time_zone = timezone(self.config["timezone"])
         self.start_time = self.time_zone.fromutc(datetime.utcnow())
-        self.img_dir = path.join(getcwd(), self.start_time.strftime("Weather Images %y%m%d"))
+        self.img_dir = path.join(
+            getcwd(), self.start_time.strftime("Weather Images %y%m%d"))
         if not path.exists(self.img_dir):
             mkdir(self.img_dir)
         self.download()
@@ -30,26 +33,30 @@ class downloader(object):
         for item in self.config["items"]:
             for time in item["times"]:
                 index += 1
-                basename = ' '.join(filter(None, [f"{index:03d}", item['name'], time]))
+                basename = ' '.join(
+                    filter(None, [f"{index:03d}", item['name'], time]))
                 if len(glob(f"{path.join(self.img_dir, basename)}*")) == 0:
                     try:
                         url_date_time = self.start_time
                         if len(time) > 0:
-                            url_date_time = url_date_time.replace(hour=int(time[0:2]), minute=int(time[2:4]))
+                            url_date_time = url_date_time.replace(
+                                hour=int(time[0:2]), minute=int(time[2:4]))
                         if len(time) > 4:
-                            url_date_time = url_date_time + timedelta(days=int(time[5:6]))
+                            url_date_time = url_date_time + \
+                                timedelta(days=int(time[5:6]))
                         if int(item["utc"]):
                             url_date_time = url_date_time - url_date_time.utcoffset()
                         url = url_date_time.strftime(item["url"])
 
                         buffer = requests.get(url)
-                        mime_type = from_buffer(buffer.content, mime=True).split('/')
+                        mime_type = from_buffer(
+                            buffer.content, mime=True).split('/')
                         extension = mime_type[1]
                         file_name = f"{basename}.{extension}"
                         file_path = path.join(self.img_dir, file_name)
                         if mime_type[0] != "image":
                             raise Exception("Not an image")
-                        
+
                         open(file_path, "wb").write(buffer.content)
 
                         print(f"Success! - {file_name}")
@@ -58,15 +65,16 @@ class downloader(object):
                         print(f"Failed to download - {str(e)} - {url}")
                         failed_items.append(f"{basename} --- {url}")
 
-        time_taken = self.time_zone.fromutc(datetime.utcnow()) - self.start_time
-        print(f"{success_count} images downloaded in {time_taken}.\n\n{len(failed_items)} images failed:")
+        time_taken = self.time_zone.fromutc(
+            datetime.utcnow()) - self.start_time
+        print(
+            f"{success_count} images downloaded in {time_taken}.\n\n{len(failed_items)} images failed:")
         for failure in failed_items:
             print(failure)
 
+
 def create_presentation():
     prs = Presentation()
-
-
 
 
 if __name__ == "__main__":
