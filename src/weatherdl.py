07a7ad58775b0e7b0a26af5f32d51132700fb158
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2022-11-27 16:00:47 +1300

    Separate config and improve error handling
    - Move configuration into its own class.
    - Reduce error handling to only continue when the image is unavailable
      and crash otherwise.

--- src/weatherdl.py
+++ src/weatherdl.py
@@ -10,74 +10,84 @@ from pytz import timezone
 from presentation import create_pptx_from_images
 
 
-class downloader(object):
+class config(object):
 
-    def __init__(self):
-        config_file = open("config.json", "r")
+    def __init__(self, config_file_path):
+        config_file = open(config_file_path, "r")
         self.config = json.loads(config_file.read())
         config_file.close()
-        self.time_zone = timezone(self.config["timezone"])
+
+        self.time_zone = timezone(self.read("time_zone"))
         self.start_time = self.time_zone.fromutc(datetime.utcnow())
-        self.img_dir = path.join(
-            getcwd(), self.start_time.strftime("Weather Images %y%m%d"))
-        if not path.exists(self.img_dir):
-            mkdir(self.img_dir)
-        self.download()
+        self.img_dir = self.start_time.strftime(self.read("working_dir"))
 
-    def download(self):
-        """
-        Download the images
-        """
-        index = 0
-        success_count = 0
-        failed_items = []
-        for item in self.config["items"]:
-            for time in item.get("times", [""]):
-                index += 1
-                basename = ' '.join(
-                    filter(None, [f"{index:03d}", item['name'], time]))
-                if len(glob(f"{path.join(self.img_dir, basename)}*")) == 0:
-                    try:
-                        url_date_time = self.start_time
-                        if len(time) > 0:
-                            url_date_time = url_date_time.replace(
-                                hour=int(time[0:2]), minute=int(time[2:4]))
-                        if len(time) > 4:
-                            url_date_time = url_date_time + \
-                                timedelta(days=int(time[5:6]))
-                        if item.get("utc"):
-                            url_date_time = url_date_time - url_date_time.utcoffset()
-                        url = url_date_time.strftime(item["url"])
+    def read(self, key):
+        return self.config[key]
 
-                        buffer = requests.get(url)
-                        mime_type = from_buffer(
-                            buffer.content, mime=True).split('/')
-                        extension = mime_type[1]
-                        file_name = f"{basename}.{extension}"
-                        file_path = path.join(self.img_dir, file_name)
-                        if mime_type[0] != "image":
-                            raise Exception("Not an image")
 
-                        file = open(file_path, "wb")
-                        file.write(buffer.content)
-                        file.close()
+def download_images(config):
+    if not path.exists(config.img_dir):
+        mkdir(config.img_dir)
 
-                        print(f"Success! - {file_name}")
-                        success_count += 1
-                    except Exception as e:
-                        print(f"Failed to download - {str(e)} - {url}")
-                        failed_items.append(f"{basename} --- {url}")
+    index = 0
+    success_count = 0
+    failed_items = []
 
-        time_taken = self.time_zone.fromutc(
-            datetime.utcnow()) - self.start_time
-        print(
-            f"{success_count} images downloaded in {time_taken}.\n")
-        if failed_items:
-            print(f"{len(failed_items)} images failed:")
-            for failure in failed_items:
-                print(failure)
+    def handle_unavailable_image(exception, basename, url):
+        print(f"Failed to download - {str(exception)} - {url}")
+        failed_items.append(f"{basename} --- {url}")
+
+    for item in config.read("items"):
+        for time in item.get("times", [""]):
+            index += 1
+            basename = ' '.join(
+                filter(None, [f"{index:03d}", item['name'], time]))
+            if len(glob(f"{path.join(config.img_dir, basename)}*")) == 0:
+                url_date_time = config.start_time
+                if len(time) > 0:
+                    url_date_time = url_date_time.replace(
+                        hour=int(time[0:2]), minute=int(time[2:4]))
+                if len(time) > 4:
+                    url_date_time = url_date_time + \
+                        timedelta(days=int(time[5:6]))
+                if item.get("utc"):
+                    url_date_time = url_date_time - url_date_time.utcoffset()
+                url = url_date_time.strftime(item["url"])
+
+                try:
+                    buffer = requests.get(url)
+                except Exception as e:
+                    handle_unavailable_image(e, basename, url)
+
+                mime_type = from_buffer(
+                    buffer.content, mime=True).split('/')
+
+                if mime_type[0] == "image":
+                    extension = mime_type[1]
+                    file_name = f"{basename}.{extension}"
+                    file_path = path.join(config.img_dir, file_name)
+
+                    file = open(file_path, "wb")
+                    file.write(buffer.content)
+                    file.close()
+
+                    print(f"Success! - {file_name}")
+                    success_count += 1
+                else:
+                    handle_unavailable_image(
+                        Exception("Not an image"), basename, url)
+
+    time_taken = config.time_zone.fromutc(
+        datetime.utcnow()) - config.start_time
+    print(
+        f"{success_count} images downloaded in {time_taken}.\n")
+    if failed_items:
+        print(f"{len(failed_items)} images failed:")
+        for failure in failed_items:
+            print(failure)
 
 
 if __name__ == "__main__":
-    dl = downloader()
-    create_pptx_from_images(datetime.now().strftime("Weather Images %y%m%d"))
+    config = config("config.json")
+    download_images(config)
+    create_pptx_from_images(config.img_dir)
