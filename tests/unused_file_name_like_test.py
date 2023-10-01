d567331ea4f6f3370a60b95dd36db34e542d8337
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-10-01 21:44:20 +1300

    Setup tests lint rules

--- tests/unused_file_name_like_test.py
+++ tests/unused_file_name_like_test.py
@@ -4,5 +4,7 @@ from presentation import unused_file_name_like
 def test_it_returns_name_if_unused():
     assert unused_file_name_like('test.pptx', []) == 'test.pptx'
 
+
 def test_it_adds_a_suffix_if_name_already_taken():
-    assert unused_file_name_like('test.pptx', ['test.pptx', 'test (1).pptx']) == 'test (2).pptx'
+    assert unused_file_name_like(
+        'test.pptx', ['test.pptx', 'test (1).pptx']) == 'test (2).pptx'
