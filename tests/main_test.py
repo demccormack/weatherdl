0648e5ffa25e8d5972a0ba7d18656a47c4c3e1c7
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-10-01 23:05:28 +1300

    Make test pass and teardown

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,16 +1,29 @@
 from os import path
+from shutil import rmtree
+
+import pytest
 
 from application_config import ApplicationConfig
 from downloader import Downloader
 from presentation import create_pptx_from_images
 
+temp_dir = path.join("tests", "test_run")
+
+
+@pytest.fixture(autouse=True)
+def teardown():
+    yield temp_dir
+
+    if path.exists(temp_dir):
+        rmtree(temp_dir)
+
 
 def test_it_creates_expected_files():
     config = ApplicationConfig(path.join("tests", "fixtures", "config.json"))
-    config.img_dir = path.join("tests", "test_run")
+    config.img_dir = temp_dir
 
     Downloader(config).run()
-    assert path.isfile(path.join("tests", "test_run", "sample.jpg"))
+    assert path.isfile(path.join(temp_dir, "001 Flying to Mt Cook.jpeg"))
 
     create_pptx_from_images(config.img_dir, config.items)
-    assert path.isfile(path.join("tests", "test_run", "briefing.pptx"))
+    assert path.isfile(path.join(temp_dir, "briefing.pptx"))
