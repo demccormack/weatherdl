fc6fba4e57f8d4c706a80404b308954e1f372ed0
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-10-01 18:39:01 +1300

    Refactor to satisfy linter complexity rule

--- src/downloader.py
+++ src/downloader.py
@@ -38,6 +38,44 @@ class Downloader:
     def includes_day_shift(self, time):
         return len(time) > 4
 
+    def url_from_time(self, item, time):
+        is_utc = item.get("utc")
+        url_from_config = item.get("url")
+
+        date_time_for_url = self.config.start_time
+        if self.is_provided(time):
+            hour = int(time[0:2])
+            minute = int(time[2:4])
+            date_time_for_url = date_time_for_url.replace(
+                hour=hour, minute=minute)
+        if self.includes_day_shift(time):
+            days = int(time[5:6])
+            date_time_for_url = date_time_for_url + \
+                timedelta(days=days)
+        if is_utc:
+            date_time_for_url -= date_time_for_url.utcoffset()
+
+        return date_time_for_url.strftime(url_from_config)
+
+    def process_buffer(self, buffer, basename, url):
+        mime_type = from_buffer(
+            buffer.content, mime=True).split('/')
+
+        if mime_type[0] == "image":
+            extension = mime_type[1]
+            file_name = f"{basename}.{extension}"
+            file_path = path.join(self.config.img_dir, file_name)
+
+            file = open(file_path, "wb")
+            file.write(buffer.content)
+            file.close()
+
+            print(f"Success! - {file_name}")
+            self.success_count += 1
+        else:
+            self.handle_unavailable_image(
+                Exception("Not an image"), basename, url)
+
     def run(self):
         img_dir = self.config.img_dir
         if not path.exists(img_dir):
@@ -46,9 +84,6 @@ class Downloader:
         index = 0
 
         for item in self.config.items:
-            is_utc = item.get("utc")
-            url_from_config = item.get("url")
-
             for time in item.get("times", [""]):
                 index += 1
 
@@ -58,23 +93,7 @@ class Downloader:
                     filter(None, [padded_slide_number, slide_name, time]))
 
                 if not self.file_exists_with_basename(basename):
-                    date_time_for_url = self.config.start_time
-
-                    if self.is_provided(time):
-                        hour = int(time[0:2])
-                        minute = int(time[2:4])
-                        date_time_for_url = date_time_for_url.replace(
-                            hour=hour, minute=minute)
-
-                    if self.includes_day_shift(time):
-                        days = int(time[5:6])
-                        date_time_for_url = date_time_for_url + \
-                            timedelta(days=days)
-
-                    if is_utc:
-                        date_time_for_url -= date_time_for_url.utcoffset()
-
-                    url = date_time_for_url.strftime(url_from_config)
+                    url = self.url_from_time(item, time)
 
                     buffer = None
                     try:
@@ -83,23 +102,7 @@ class Downloader:
                         self.handle_unavailable_image(error, basename, url)
                         continue
 
-                    mime_type = from_buffer(
-                        buffer.content, mime=True).split('/')
-
-                    if mime_type[0] == "image":
-                        extension = mime_type[1]
-                        file_name = f"{basename}.{extension}"
-                        file_path = path.join(self.config.img_dir, file_name)
-
-                        file = open(file_path, "wb")
-                        file.write(buffer.content)
-                        file.close()
-
-                        print(f"Success! - {file_name}")
-                        self.success_count += 1
-                    else:
-                        self.handle_unavailable_image(
-                            Exception("Not an image"), basename, url)
+                    self.process_buffer(buffer, basename, url)
 
         time_taken = self.config.time_zone.fromutc(
             datetime.utcnow()) - self.config.start_time
